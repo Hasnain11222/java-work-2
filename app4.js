95247f69482519c635179a520c0442ccfe79850b
Hasnain11222 <109806090+Hasnain11222@users.noreply.github.com>
2022-09-01 02:54:07 +0500

    Initial commit

---
+++ app4.js
@@ -0,0 +1,56 @@
+// let getresult = () => {
+
+// let textvalue = document.getElementById("txt").value
+// document.getElementById("answer").innerHTML = textvalue;
+
+// }
+
+
+// for (let i = 0; i <=20; i++){
+//  console.log(i);
+// }
+
+
+
+
+// let i = 0;
+// while (i <=10) {
+//        console.log(i);
+//              i++
+// }
+
+
+// let i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// }
+// while (i <=20);
+
+
+// let data =["ali","sameer","hasnain","mustafa","saad"];
+
+// for(let i = 0; i <=4; i++){
+//  console.log(data[i]);
+// }
+
+
+
+
+// let data =["ali","sameer","hasnain","mustafa","saad"];
+// let i = 0;
+// while(i <=4){
+// console.log(data[i]);
+// i++
+// }
+
+
+
+
+let data =["ali","sameer","hasnain","mustafa","saad"];
+let i = 0;
+do{
+console.log(data[i]);
+i++;
+}
+while(i <= 4)
